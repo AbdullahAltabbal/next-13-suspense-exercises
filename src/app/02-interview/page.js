6f8f44b8e5133b283f93e017e30bfb2c64294818
Abdullah Al Tabbal <xlp1abode@gmail.com>
2023-12-22 14:11:43 +0100

    resolve comments

--- src/app/02-interview/page.js
+++ src/app/02-interview/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { getComments } from '@/helpers/interview-helpers';
 import Comment from '@/components/Comment';
+import Spinner from '@/components/Spinner';
 
 export const dynamic = 'force-dynamic';
 
@@ -126,15 +127,20 @@ async function InterviewExercise() {
 
       <section className="comments-section">
         <h2>Discussion</h2>
-        {comments.map((comment) => (
-          <Comment
-            key={comment.id}
-            comment={comment}
-          />
-        ))}
+        <React.Suspense fallback={<Spinner />}>
+          <Comments />
+        </React.Suspense>
       </section>
     </>
   );
+}
+
+async function Comments() {
+  const comments = await getComments();
+
+  return comments.map((comment) => (
+    <Comment key={comment.id} comment={comment} />
+  ));
 }
 
 export default InterviewExercise;
